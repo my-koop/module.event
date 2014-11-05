a71be4c3b222edfa04b321a0904e0230d72f36a6
Cellule <mike.ferris@hotmail.com>
2014-11-05 15:40:38 -0500

    Base files from template

---
+++ README.md
@@ -0,0 +1,4 @@
+module.event
+===========
+
+// FIXME:: Add description
---
+++ lib/classes/ModuleClass1.ts
@@ -0,0 +1,5 @@
+class ModuleClass1 implements mkevent.ModuleClass1 {
+  value: string;
+  id: number;
+}
+export = ModuleClass1;
