8d2725c0940edb5e848c91444b11062c0eb243d7
wsalame <wsalame89@gmail.com>
2014-11-10 19:18:51 -0500

    Creation of Events

---
+++ components/Events.jsx
@@ -0,0 +1,14 @@
+var React             = require("react");
+var BSCol             = require("react-bootstrap/Col");
+var BSButton          = require("react-bootstrap/Button");
+var MKIcon            = require("mykoop-core/components/Icon");
+var MKTableSorter     = require("mykoop-core/components/TableSorter");
+var MKListModButtons  = require("mykoop-core/components/ListModButtons");
+var __                = require("language").__;
+var actions           = require("actions");
+
+var Events = React.createClass({
+  
+});
+
+module.exports = Events;
