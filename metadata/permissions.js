8a8da51c2b9be4eddf27110fdfa9e6f259ef631a
Mathieu M-Gosselin <mathieumg@gmail.com>
2014-12-07 09:52:43 -0500

    Further work on implementing permissions.

---
+++ metadata/permissions.js
@@ -0,0 +1,21 @@
+var permissions = {
+    events: {
+        create: true,
+        view: true,
+        update: true,
+        delete: true,
+        control: true,
+        cancel: true,
+        register: true,
+        notes: {
+            view: true,
+            create: true
+        },
+        users: {
+            view: true,
+            add: true,
+            remove: true
+        }
+    }
+};
+module.exports = permissions;
